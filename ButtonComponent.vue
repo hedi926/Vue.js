dd3a75612ab6fdc179fe60427e1371ab54d02b05
hedi926 <66388098+hedi926@users.noreply.github.com>
2025-02-13 12:21:33 +0100

    Add files via upload
    Ajout d'un bouton en vue

---
+++ ButtonComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <button
+     :type="data.type"
+     :id="data.id"
+     :class="data.class">
+    </button>
+  </template>
+<script setup lang="ts">
+
+interface ButtonComponentProperties {
+    data: {
+        id: string;
+        type?: "button" | "submit" | "reset";
+        class?: string;
+    }
+} 
+
+defineProps<ButtonComponentProperties>();
+
+</script>
+
---
+++ HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="home-container">
+        <h1>Home Page</h1>
+        <ButtonComponent :data="{id:'submit-btn', type:'submit', class:'bg-red-600 p-4 rounded-lg'}" />
+        
+    </main>
+    </template>
+    
+    <script lang="ts" setup>
+
+    import ButtonComponent from '../components/ButtonComponent.vue';
+
+    </script>
+
+    <style>
+    </style>
